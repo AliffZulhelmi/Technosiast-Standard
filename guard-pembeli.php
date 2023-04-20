308f54cd3f30409b4848868bba1f472398b55e4c
Aliff Zulhelmi | 0xD0z <90973479+AliffZulhelmi@users.noreply.github.com>
2023-04-21 00:05:15 +0800

    FINAL PRODUCT!

--- guard-pembeli.php
+++ guard-pembeli.php
@@ -1,9 +1,15 @@
-<?php
-# Menyemak nilai pembolehubah session['tahap']
-if($_SESSION['tahap'] != 'pembeli')
+<?php 
+
+# Menyemak nilai pembolehubah session ['tahap']
+if(empty($_SESSION))
 {
-    # Jika nilainya tidak sama dengan pembeli. Aturcara akan dihentikan
-    die('<script>alert("sila login");
-    window.location.href="logout.php";</script>');
+    die("<script>alert('sila login');
+    window.location.href='logout.php';</script>");
+}
+elseif($_SESSION['tahap'] != "pembeli")
+{
+    # Jika nilainya tidak sama dengan pembeli. aturcara akan dihentikan
+    die("<script>alert('sila login');
+    window.location.href='logout.php';</script>");
 }
 ?>
--- guard-staff.php
+++ guard-staff.php
@@ -1,6 +1,12 @@
 <?php 
+
 # Menyemak nilai pembolehubah session ['tahap']
-if($_SESSION['tahap'] != "staff")
+if(empty($_SESSION))
+{
+    die("<script>alert('sila login');
+    window.location.href='logout.php';</script>");
+}
+elseif($_SESSION['tahap'] != "staff")
 {
     # Jika nilainya tidak sama dengan staff. aturcara akan dihentikan
     die("<script>alert('sila login');
